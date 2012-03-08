511d698fed952eaae8247c5736eaae4462ecea37
Mauricio Lobo <mauriciolobo@gmail.com>
2012-03-08 17:43:52 -0300

    Hand Implmented. Next should be CardMixer

--- TCG.Tests/HandTests.cs
+++ TCG.Tests/HandTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace TCG.Tests
@@ -9,10 +10,94 @@ namespace TCG.Tests
     [TestFixture]
     public class HandTests
     {
-        [Test]
-        public void JustToKnowWhereToStart()
+        private List<Player> players;
+
+        [SetUp]
+        public void TestStartUp()
         {
-            Assert.Fail("Should start create a Hand tests. Read the README to know the Hand behavior.");
+            players = new List<Player>()
+                          {
+                              new Player("PlayerOne"),
+                              new Player("PlayerTwo")
+                          };
+        }
+
+        [Test]
+        public void StartingAHand()
+        {
+            var hand = new Hand(players);
+
+            hand.IsOver.Should().BeFalse();
+        }
+
+        [Test]
+        public void ShouldBeTheFirstPlayerToStartAHand()
+        {
+            var hand = new Hand(players);
+
+            hand.ActivePlayer.Should().Be(players[0]);
+        }
+
+        [Test]
+        public void Scored3PointsAtVictory()
+        {
+            var hand = new Hand(players);
+
+            hand.Winner(players[0]);
+
+            hand.Score(players[0]).Should().Be(3);
+        }
+
+        [Test]
+        public void HandEndAfterTheSamePlayerWin2TimesInARow()
+        {
+            var hand = new Hand(players);
+
+            hand.Winner(players[0]);
+            hand.Winner(players[0]);
+
+            hand.IsOver.Should().BeTrue();
+        } 
+       
+        [Test]
+        public void HandWhenAPlayerWinAfterADraw()
+        {
+            var hand = new Hand(players);
+
+            hand.Draw();
+            hand.Winner(players[0]);
+
+            hand.IsOver.Should().BeTrue();
+        }   
+     
+        [Test]
+        public void PlayerWinAfterWinLoseWin()
+        {
+            var hand = new Hand(players);
+
+            hand.Winner(players[0]);
+            hand.Winner(players[1]);
+            hand.Winner(players[0]);
+
+            hand.GetWinner().Should().Be(players[0]);
+        }
+
+        [Test]
+        public void ThrowTrucoExceptionWhenTryToGetWinnerBeforeHandIsOver()
+        {
+            var hand = new Hand(players);
+
+            hand.Winner(players[0]);
+            hand.Winner(players[1]);
+            try
+            {
+                hand.GetWinner().Should().Be(players[0]);
+                Assert.Fail("Should throw an exception if try to get winner before hand has over");
+            }
+            catch (TrucoException ex)
+            {
+                ex.Message.Should().Be("Cannot define a winner before the hand has over");
+            }
         }
     }
 }
---
+++ TCG/Hand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TCG
+{
+    public class Hand
+    {
+        private List<Player> players;
+        private Dictionary<Player, int> scoreBoard = new Dictionary<Player, int>();
+
+        public Hand(List<Player> players)
+        {            
+            this.players = players;
+            ActivePlayer = players[0];
+            StartScoreBoard();
+        }
+
+        private void StartScoreBoard()
+        {
+            foreach (var player in players)            
+                scoreBoard.Add(player,0);            
+        }
+
+        public bool IsOver { get; set; }
+
+        public Player ActivePlayer { get; set; }
+
+        public void Winner(Player player)
+        {
+            SetScore(player, 3);
+        }
+
+        private void SetScore(Player player, int score)
+        {
+            scoreBoard[player] += score;
+
+            IsOver = scoreBoard.Count(x => x.Value > 3) > 0;
+        }
+
+        public int Score(Player player)
+        {
+            return scoreBoard[player];
+        }
+
+        public void Draw()
+        {
+            foreach (var player in players)            
+                SetScore(player,1);            
+        }
+
+        public Player GetWinner()
+        {
+            if(!IsOver)
+                throw new TrucoException("Cannot define a winner before the hand has over");
+
+            return scoreBoard.First(x => x.Value > 3).Key;
+        }
+    }
+}
